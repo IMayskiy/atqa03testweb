896c11f3a4f3beb96db09a6d8a98152aeb6c0c55
Igor R <igrasputin@yandex.ru>
2023-07-03 22:50:39 +0300

    Test commit

---
+++ src/test/java/ru/netology/webselenide/WebOrderTest.java
@@ -0,0 +1,29 @@
+package ru.netology.webselenide;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
+//import static com.codeborne.selenide.Selenide.open;
+
+class WebOrderTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldTest () {
+        SelenideElement form = $("[class]");
+        form.$("[data-test-id=name] input").setValue("Петров Олег");
+        form.$("[data-test-id=phone] input").setValue("+79091234567");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+//        Thread.sleep(500);
+    }
+}
